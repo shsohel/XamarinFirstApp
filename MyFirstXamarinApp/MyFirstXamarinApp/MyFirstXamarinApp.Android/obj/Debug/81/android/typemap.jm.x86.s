45485c5aed8b21d27eec5f2d5767a43d6ccbfcec
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2019-10-05 21:31:32 +0600

    Xamarian First App
    dd

---
+++ MyFirstXamarinApp/MyFirstXamarinApp/MyFirstXamarinApp.Android/obj/Debug/81/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 02ed2d4aa24eaebc5d6a58961d694d47e18e98c1 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1100
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 288201
+	.include	"typemap.jm.inc"
